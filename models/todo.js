fd76a8acf3cfddaa599576c6c0746c16cc80f200
Mitesh073 <113135872+Mitesh073@users.noreply.github.com>
2022-12-26 21:48:19 +0530

    Update todo.js

--- models/todo.js
+++ models/todo.js
@@ -1,118 +1,75 @@
-/* eslint-disable no-unused-vars */
-/* eslint-disable no-undef */
 "use strict";
 const { Model, Op } = require("sequelize");
+
 module.exports = (sequelize, DataTypes) => {
   class Todo extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
     static associate(models) {
       // define association here
     }
-
-    static async showList() {
-      console.log("My Todo list \n");
-
-      console.log("Overdue");
-
-      console.log(
-        (await Todo.overdue())
-          .map((x) => {
-            x.displayableString();
-          })
-          .join("\n")
-      );
-
-      console.log("\n");
-
-      console.log("Due Today");
-
-      console.log(
-        (await Todo.dueToday())
-          .map((x) => {
-            x.displayableString();
-          })
-          .join("\n")
-      );
-
-      console.log("\n");
-
-      console.log("Due Later");
-
-      console.log(
-        (await Todo.dueLater())
-          .map((x) => {
-            x.displayableString();
-          })
-          .join("\n")
-      );
-    }
-
-    static async overdue() {
-      return await Todo.findAll({
-        where: {
-          dueDate: { [Op.lt]: new Date().toLocaleDateString("en-CA") },
-          completed: false,
-        },
-        order: [["id", "ASC"]],
-      });
-    }
-
-    static async dueToday() {
-      return await Todo.findAll({
-        where: {
-          dueDate: { [Op.eq]: new Date().toLocaleDateString("en-CA") },
-          completed: false,
-        },
-        order: [["id", "ASC"]],
-      });
-    }
-
-    static async dueLater() {
-      return await Todo.findAll({
-        where: {
-          dueDate: { [Op.gt]: new Date().toLocaleDateString("en-CA") },
-          completed: false,
-        },
-        order: [["id", "ASC"]],
-      });
-    }
-
-    static async completedTodo() {
-      return await Todo.findAll({
-        where: {
-          completed: true,
-        },
-      });
-    }
-
-    static getTodos() {
-      return this.findAll();
-    }
-
     static addTodo({ title, dueDate }) {
       return this.create({ title: title, dueDate: dueDate, completed: false });
     }
-
-    setCompletionStatus(completed) {
-      return this.update({ completed: completed });
+    markAsCompleted() {
+      return this.update({ completed: true });
     }
-
-    static async remove(id) {
-      return this.destroy({
+    deletetodo() {
+      return this.removetask(id);
+    }
+    static getTodos() {
+      return this.findAll({ order: [["id", "ASC"]] });
+    }
+    static overdue() {
+      return this.findAll({
         where: {
-          id: id,
+          dueDate: {
+            [Op.lt]: new Date(),
+          },
         },
       });
     }
-
-    displayableString() {
-      let checkbox = this.completed ? "[x]" : "[ ]";
-      return `${this.id}. ${checkbox} ${this.title} ${
-        this.dueDate == new Date().toLocaleDateString("en-CA")
-          ? ""
-          : this.dueDate
-      }`.trim();
+    static dueToday() {
+      return this.findAll({
+        where: {
+          dueDate: {
+            [Op.eq]: new Date(),
+          },
+        },
+      });
+    }
+    static dueLater() {
+      return this.findAll({
+        where: {
+          dueDate: {
+            [Op.gt]: new Date(),
+          },
+        },
+      });
+    }
+    static completedItems() {
+      return this.findAll({
+        where: {
+          completed: true,
+        },
+        order: [["id", "ASC"]],
+      });
+    }
+    static async remove(id) {
+      return this.destroy({
+        where: {
+          id,
+        },
+      });
+    }
+    setCompletionStatus(bool) {
+      return this.update({ completed: bool });
     }
   }
+
   Todo.init(
     {
       title: DataTypes.STRING,
